caf815bb759b7c4e97f96bdf8c16becfb6af71bc
vmqxdv <vquintilio@gmail.com>
2025-04-01 17:32:35 +0200

    feat: Aggiunto messaggio di errore e corretto posizionamento

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 1229;
 const router = require('./routes/posts');
+
 const notFound = require('./middlewares/notFound');
 
 
 app.use(express.static('public'));
+app.use(express.json());
+
+
+app.use('/posts', router);
 
 app.use(notFound);
-app.use(express.json());
-app.use('/posts', router);
 
 app.listen(PORT, () => {
   console.log(`Server avviato su http://localhost:${PORT}`);
--- src/middlewares/notFound.js
+++ src/middlewares/notFound.js
@@ -1,5 +1,5 @@
 function notFound(req, res, next) {
-
+  return res.status(404).json({ error: 'Endpoint non trovato' });
 };
 
 
